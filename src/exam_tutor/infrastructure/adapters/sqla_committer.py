8dae49223fb779732be4191364d24304374ee94f
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-12-20 16:53:19 +0500

    Create: adapter for committer and infra

---
+++ src/exam_tutor/infrastructure/adapters/sqla_committer.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from exam_tutor.application.interfaces.committer import Committer
+
+
+class Committer(Committer):
+    def __init__(self, session: AsyncSession):
+        self._session = session
+
+    async def commit(self) -> None:
+        await self._session.commit()
