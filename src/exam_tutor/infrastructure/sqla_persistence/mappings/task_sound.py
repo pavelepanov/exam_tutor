c0e0e7d688f7d26cca5ffc415451acd76c7b7f5b
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-12-22 19:18:10 +0500

    Create: task sound table

---
+++ src/exam_tutor/infrastructure/sqla_persistence/mappings/task_sound.py
@@ -0,0 +1,10 @@
+from sqlalchemy import UUID, Column, String, Table
+
+from exam_tutor.infrastructure.sqla_persistence.orm_registry import metadata_obj
+
+task_sounds_table = Table(
+    "task_sounds",
+    metadata_obj,
+    Column("id", UUID(as_uuid=True), primary_key=True),
+    Column("link", String),
+)
