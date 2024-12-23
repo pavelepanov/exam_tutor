b193b25c6d92a9416f072f807248d2f4bd78cb55
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-12-23 18:09:27 +0500

    Rename and update: delete flush and rename to CommitterImpl

--- src/exam_tutor/infrastructure/adapters/sqla_committer.py
+++ src/exam_tutor/infrastructure/adapters/sqla_committer.py
@@ -3,12 +3,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from exam_tutor.application.interfaces.committer import Committer
 
 
-class Committer(Committer):
+class CommitterImpl(Committer):
     def __init__(self, session: AsyncSession):
         self._session = session
 
     async def commit(self) -> None:
         await self._session.commit()
-
-    async def flush(self):
-        await self._session.flush()
