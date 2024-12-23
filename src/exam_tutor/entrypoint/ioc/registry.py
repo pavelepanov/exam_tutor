53c3ac05c3ba8fc43403345a8ec65a7883375c04
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-12-23 19:15:28 +0500

    Update: startup project

--- src/exam_tutor/entrypoint/ioc/registry.py
+++ src/exam_tutor/entrypoint/ioc/registry.py
@@ -1,9 +1,18 @@
+from typing import Iterable
+
 from dishka import AsyncContainer, Provider, Scope, make_async_container, provide
+
 from exam_tutor.entrypoint.ioc.adapters import SqlaProvider
 from exam_tutor.entrypoint.ioc.interactors import InteractorProvider
 
 
 def create_async_container() -> AsyncContainer:
-    return make_async_container(
-        SqlaProvider(), InteractorProvider()
+    return make_async_container(SqlaProvider(), InteractorProvider())
+
+
+def get_providers() -> Iterable[Provider]:
+    return (
+        SqlaProvider(),
+        InteractorProvider(),
+
     )
