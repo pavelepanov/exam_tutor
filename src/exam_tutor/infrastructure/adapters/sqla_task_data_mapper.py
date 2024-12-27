d578b82c449d54121ed09f18b0326dc1c2ace578
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-12-27 23:20:30 +0500

    Update: work with sqla

--- src/exam_tutor/infrastructure/adapters/sqla_task_data_mapper.py
+++ src/exam_tutor/infrastructure/adapters/sqla_task_data_mapper.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql.operators import eq
 
 from exam_tutor.application.interfaces.task_data_gateway import TaskDataGateway
-from exam_tutor.domain.entities.task import Task, TaskId
+from exam_tutor.domain.entities.task import Task, FindCode
 from exam_tutor.infrastructure.sqla_persistence.mappings.task import tasks_table
 
 
@@ -11,9 +11,10 @@ class SqlaTaskDataMapper(TaskDataGateway):
     def __init__(self, session: AsyncSession) -> None:
         self._session = session
 
-    async def read_by_uuid(self, id: TaskId) -> Task | None:
-        stmt = select(tasks_table).where(eq(tasks_table.id, id))
+    async def read_by_find_code(self, find_code: FindCode) -> Task | None:
+        stmt = select(Task).where(tasks_table.c.find_code == find_code)
 
-        task: Task | None = await self._session.execute(stmt).scalar_one_or_none()
+        result = await self._session.execute(stmt)
+        task: Task | None = result.scalars().one_or_none()
 
         return task
