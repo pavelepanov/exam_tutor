75d0cd2995a853b28a55ac8c0bdf9b6cc5fbc361
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-12-28 18:19:11 +0500

    Update: change to add from create_task

--- src/exam_tutor/application/interfaces/task_data_gateway.py
+++ src/exam_tutor/application/interfaces/task_data_gateway.py
@@ -1,22 +1,7 @@
 from abc import abstractmethod
-from datetime import datetime
 from typing import Protocol
 
-from exam_tutor.domain.entities.task import (
-    AnswerVideoLink,
-    Condition,
-    DifficultEnum,
-    ExamEnum,
-    ExamTaskNumber,
-    FindCode,
-    StrAnswer,
-    SubjectEnum,
-    Task,
-    TaskFileLink,
-    TaskId,
-    TaskPhotoLink,
-    TaskSoundLink,
-)
+from exam_tutor.domain.entities.task import FindCode, Task
 
 
 class TaskDataGateway(Protocol):
@@ -24,19 +9,4 @@ class TaskDataGateway(Protocol):
     async def read_by_find_code(self, find_code: FindCode) -> Task | None: ...
 
     @abstractmethod
-    async def create_task(
-        self,
-        id: TaskId,
-        exam: ExamEnum,
-        subject: SubjectEnum,
-        exam_task_number: ExamTaskNumber,
-        condition: Condition,
-        answer: StrAnswer,
-        difficult: DifficultEnum,
-        created_at: datetime,
-        find_code: FindCode,
-        task_sound_link: TaskSoundLink | None = None,
-        task_file_link: TaskFileLink | None = None,
-        task_photo_link: TaskPhotoLink | None = None,
-        answer_video_link: AnswerVideoLink | None = None,
-    ) -> Task: ...
+    async def add(self, task: Task) -> Task: ...
