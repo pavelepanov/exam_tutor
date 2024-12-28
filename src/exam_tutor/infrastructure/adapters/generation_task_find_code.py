dae929d8f7f8c764658019c58ea28deacc4e57f2
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-12-28 17:43:14 +0500

    Create: adapter for generation_task_find_code

---
+++ src/exam_tutor/infrastructure/adapters/generation_task_find_code.py
@@ -0,0 +1,17 @@
+from random import choices
+from string import ascii_letters, digits
+
+from exam_tutor.application.constants import TASK_FIND_CODE_LENGTH
+from exam_tutor.application.interfaces.generation_task_find_code import (
+    GenerationTaskFindCode,
+)
+from exam_tutor.domain.entities.task import FindCode
+
+
+class GenerationTaskFindCodeImpl(GenerationTaskFindCode):
+    async def generate_task_find_code(self) -> FindCode:
+        characters = ascii_letters + digits
+        find_code: FindCode = FindCode(
+            "".join(choices(characters, k=TASK_FIND_CODE_LENGTH))
+        )
+        return find_code
