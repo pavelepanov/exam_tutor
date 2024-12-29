10b2e8cc177a0c1ea20ac9be060d1f397a797867
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-12-29 22:00:23 +0500

    Update: add minio

--- src/exam_tutor/application/interfaces/file_manager.py
+++ src/exam_tutor/application/interfaces/file_manager.py
@@ -1,16 +1,9 @@
 from abc import abstractmethod
 from typing import Protocol
 
-from exam_tutor.domain.entities.task import TaskId
-
 
 class FileManager(Protocol):
     @abstractmethod
-    async def save(self, payload: bytes, path: str) -> None: ...
-
-    @abstractmethod
-    async def get_by_file_link(self, file_path: str) -> bytes | None: ...
-
-
-def create_file_path(task_id: TaskId, extension: str) -> str:
-    return f"{task_id}.{extension}"
+    async def save_answer_video(
+        self, payload: bytes, answer_video_link: str, content_type: str
+    ) -> None: ...
