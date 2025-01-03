845a7962e941e1b6175397b451f4bb6c234ed69d
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2025-01-04 00:02:57 +0500

    Update: change link enum

--- src/exam_tutor/infrastructure/adapters/generation_answer_video_link.py
+++ src/exam_tutor/infrastructure/adapters/generation_answer_video_link.py
@@ -1,7 +1,9 @@
 from random import choices
 from string import ascii_letters, digits
 
-from exam_tutor.application.constants import TASK_ANSWER_VIDEO_LINK_LENGTH
+from exam_tutor.application.constants import (
+    TASK_ANSWER_VIDEO_LINK_WITHOUT_PREFIX_LENGTH,
+)
 from exam_tutor.domain.entities.task import AnswerVideoLink
 from exam_tutor.domain.interfaces.generation_task_answer_video_link import (
     GenerationAnswerVideoLink,
@@ -13,6 +15,8 @@ class GenerationAnswerVideoLinkImpl(GenerationAnswerVideoLink):
         characters = ascii_letters + digits
         answer_video_link: AnswerVideoLink = AnswerVideoLink(
             "answer_video"
-            + "".join(choices(characters, k=TASK_ANSWER_VIDEO_LINK_LENGTH))
+            + "".join(
+                choices(characters, k=TASK_ANSWER_VIDEO_LINK_WITHOUT_PREFIX_LENGTH)
+            )
         )
         return answer_video_link
