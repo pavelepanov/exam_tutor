66b8c3c234bd975280c26546c60a87fb352ca1f1
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2025-01-04 14:23:22 +0500

    Create: methods for work with file_manager

--- src/exam_tutor/application/interfaces/file_manager.py
+++ src/exam_tutor/application/interfaces/file_manager.py
@@ -1,9 +1,26 @@
 from abc import abstractmethod
 from typing import Protocol
 
+from exam_tutor.domain.entities.task import TaskFileLink, TaskPhotoLink, TaskSoundLink
+
 
 class FileManager(Protocol):
     @abstractmethod
     async def save_answer_video(
         self, payload: bytes, answer_video_link: str, content_type: str
+    ) -> None: ...
+
+    @abstractmethod
+    async def save_sound_file(
+        self, file_info: dict[str, bytes | str | TaskSoundLink]
+    ) -> None: ...
+
+    @abstractmethod
+    async def save_file_file(
+        self, file_info: dict[str, bytes | str | TaskFileLink]
+    ) -> None: ...
+
+    @abstractmethod
+    async def save_photo_file(
+        self, file_info: dict[str, bytes | str | TaskPhotoLink]
     ) -> None: ...
